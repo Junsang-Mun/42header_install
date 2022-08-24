c8075df33af593f82894eb4c24218450a58d7c3f
Junsang <mun.js@aol.com>
2022-08-25 07:18:37 +0900

    Update gcc_flag_alias.sh

--- gcc_flag_alias.sh
+++ gcc_flag_alias.sh
@@ -3,6 +3,8 @@
 echo "이 스크립트는 gccw라는 별칭을 만듭니다."
 echo "gccw를 실행하면 gcc -Werror -Wextra -Wall 과 동일하게 작동합니다."
 
+echo '' >> ~/.zshrc
+echo '# gccw 별칭 설정: gcc -Werror -Wextra -Wall' >> ~/.zshrc
 echo alias gccw='gcc -Werror -Wextra -Wall' >> ~/.zshrc
 
 source ~/.zshrc
